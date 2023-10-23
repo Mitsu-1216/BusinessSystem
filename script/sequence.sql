bdbd60ae336358a006643902a5be7f634a1c299b
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-10-24 08:19:49 +0900

    シーケンスリセット

--- script/sequence.sql
+++ script/sequence.sql
@@ -1,6 +1,11 @@
+--シーケンス作成
 CREATE SEQUENCE CUSTOMER_ID_SEQ
     INCREMENT BY 1
     MAXVALUE 99999999
     START WITH 1
     NO CYCLE
 ;
+
+--シーケンスリセット
+--6の次からスタート
+SELECT SETVAL ('customer_id_seq', 6, true);
