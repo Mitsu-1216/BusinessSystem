2226e742ceba87a13252c1314e75d9cc1fac2dd3
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-11-04 15:31:33 +0900

    外部キー追加

--- script/attribute.sql
+++ script/attribute.sql
@@ -3,9 +3,12 @@ ALTER TABLE customer_table
 ADD FOREIGN KEY (job) 
 REFERENCES m_job (job_id);
 
-
-
 --出身地
 ALTER TABLE customer_table
 ADD FOREIGN KEY (birthplace) 
 REFERENCES m_prefecture (prefecture_id);
+
+--購入テーブル
+ALTER TABLE purchase_table
+ADD FOREIGN KEY (customer_id) 
+REFERENCES customer_table (customer_id);
